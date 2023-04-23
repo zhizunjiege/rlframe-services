92447df0d22a36fa1137ed0032134da4a334c4ea
LaPluma030 <1308677911@qq.com>
2023-04-23 12:50:44 +0800

    update agent.py&init

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,11 @@ from .dqn import DQN
 from .double_dqn import DoubleDQN
 
 from .ddpg import DDPG
+from .maddpg import MADDPG
 
 RLModels = {
     'DQN': DQN,
     'DoubleDQN': DoubleDQN,
     'DDPG': DDPG,
+    'MADDPG': MADDPG,
 }
