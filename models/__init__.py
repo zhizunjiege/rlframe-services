bfb3e46d0ffab24e1712e7fdc2c26c5caffbef73
zhizunjiege <2452816044@qq.com>
2023-10-14 14:57:27 +0800

    fix: ppo not imported

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,14 @@
 from .dqn import DQN
 from .doubledqn import DoubleDQN
-
 from .ddpg import DDPG
+from .ppo import PPO
+
 from .maddpg import MADDPG
 
 RLModels = {model.__name__: model for model in [
     DQN,
     DoubleDQN,
     DDPG,
+    PPO,
     MADDPG,
 ]}
